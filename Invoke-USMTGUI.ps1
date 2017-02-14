0900ec063490eefde6224678e0bd4770a3952eeb
Nick Rodriguez <Nick.Rodriguez@dhgllp.com>
2017-02-14 14:55:54 -0500

    Fixed admin test function. Break was breaking script.

--- Invoke-USMTGUI.ps1
+++ Invoke-USMTGUI.ps1
@@ -127,11 +127,11 @@ begin {
     }
 
     function Test-IsAdmin {
-        if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-            [Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+        $UserIdentity = [Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+
+        if (-not $UserIdentity.IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
             Update-Log "You are not running this script with an admin account. " -Color 'Red' -NoNewLine
             Update-Log "Some tasks may fail if not run with admin credentials.`n" -Color 'Red'
-            Break
         }
     }
 
